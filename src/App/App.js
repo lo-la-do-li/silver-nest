25d04125d16c5989b767c5575c6547b77647ba89
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-05 23:45:51 -0700

    Create component folders/files

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is SilverNest. 
+    </div>
+  );
+}
+
+export default App;

ab27e09dd4875a82c68c48bf05a16777c0b1f8b9
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-06 21:21:36 -0700

    Install react-router-dom; Set up basic  router

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect} from 'react';
 import './App.scss';
 import NavBar from '../NavBar/NavBar';
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      This is SilverNest. 
+      <header>
+        <h1>Silver Nest</h1>
+      </header>
+      <NavBar />
+      <Switch>
+        <Route path="/about" />
+        <Route path="/get-involved" />
+      </Switch>
     </div>
   );
 }

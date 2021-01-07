696be6d6782b95bf2ed5646b8efe9315211c4d80
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-07 00:15:47 -0700

    Add GridScroll to Thumbnail Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,23 @@
 import React, { useState, useEffect} from 'react';
 import './App.scss';
 import NavBar from '../NavBar/NavBar';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import SingleLineGridList from '../Thumbnail/Thumbnail'
 
 function App() {
   return (
+
     <div className="App">
-      <header>
-        <h1>Silver Nest</h1>
-      </header>
+      <Box component="span" m={1}>
+        <header>
+          <h1>Silver Nest</h1>
+        </header>
+      </Box>
       <NavBar />
       <Switch>
         <Route path="/about" />
-        <Route path="/get-involved" />
+        <Route path="/get-involved" component={SingleLineGridList}/>
       </Switch>
     </div>
   );

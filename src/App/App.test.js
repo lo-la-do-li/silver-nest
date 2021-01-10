aee46c4cfd726f6c69b70cbeff9bfa9b4572f752
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-09 18:13:15 -0700

    Testing setup for app component

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import { Router, MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it.skip('It should display a header', () => {
+    
+  })
+  it.skip('It should display a navBar', () => {
+    
+  })
+  it.skip('It should display about details on load', () => {
+    
+  })
+  it.skip('It should display FindARoommate component when Find A Roommate tab is clicked', () => {
+  
+  })
+  //This testing file focuses more on integration testing
+})

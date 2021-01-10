afb6e583a1c9e3c5b8ecd85cee83a06c7aa1188a
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-09 18:15:04 -0700

    Testing setup for About component

---
+++ src/About/About.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import About from './About';
+import { Router, MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
+
+describe('About', () => {
+  it.skip('It should display a description of the app and the collaboration', () => {
+    
+  })
+})

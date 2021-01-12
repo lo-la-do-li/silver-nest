294a2a06b7541620d0dc98e0cfc0350edec0f7f6
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-11 20:44:28 -0700

    "Create and pass tests for Form component"

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Form from './Form';
-import { Router, MemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { createMemoryHistory } from "history";
 
 describe('Form', () => {
-  //this file will mostly be for unit testing
-  it.skip('It should display a form', () => {
-    // setup and execution 
+  it('It should display a form', () => {
+    const mockSelectedSemester = jest.fn()
     render(
-
+      <MemoryRouter>
+        <Form
+          selectedSemester={mockSelectedSemester}
+        />
+      </MemoryRouter>
     )
-    //execution
 
+    const formText = screen.getByText('Choose a semester')
     // assertions
-    expect(screen.getByText()).toBeInTheDocument()
+    expect(formText).toBeInTheDocument()
+  })
+
+  it('It should display a form', async () => {
+    const mockSelectedSemester = jest.fn()
+    const history = createMemoryHistory()
+    
+    render(
+      <MemoryRouter>
+        <Form
+          selectedSemester={mockSelectedSemester}
+        />
+      </MemoryRouter>
+    )
+    
+    const chooseSemesterForm = screen.getByRole('button')
+    
+    userEvent.click(chooseSemesterForm)
+    
+    const springText = screen.getByRole('option', { name: /summer 2021/i })
+    const summmerText = screen.getByText('Summer 2021')
+    const fallText = screen.getByText('Fall 2021')
+    
+    expect(springText).toBeInTheDocument()
+    expect(summmerText).toBeInTheDocument()
+    expect(fallText).toBeInTheDocument()
   })
 })

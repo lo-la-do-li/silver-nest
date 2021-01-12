6c6366775c1ecfbd71a484ca9a699099d0ff143d
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-11 19:59:15 -0700

    "Write and pass tests for NavBar"

--- src/NavBar/NavBar.test.js
+++ src/NavBar/NavBar.test.js
@@ -1,20 +1,55 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NavBar from './NavBar';
 import { Router, MemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from "history";
 
 describe('NavBar', () => {
-  //This test file will mostly be for unit testing
-  it.skip('It should display a description of the app and the collaboration', () => {
-    // setup and execution 
+  it('should render navbar with About and Find A Roommate', () => {
     render(
-
+      <MemoryRouter>
+        <NavBar/>
+      </MemoryRouter>
     )
-    //execution
-      
-    // assertions
-    expect(screen.getByText()).toBeInTheDocument()
+  
+    const aboutTab = screen.getByText('About')
+    const findRoommateTab = screen.getByText('Find A Roommate')
+  
+    expect(aboutTab).toBeInTheDocument()
+    expect(findRoommateTab).toBeInTheDocument()
+  })
+
+  it('should route user to About page when About tab is clicked', async () => {
+    const history = createMemoryHistory()
+
+    render(
+      <Router history={history}>
+        <NavBar/>
+      </Router>
+    )
+
+    const aboutTab = await waitFor(() => screen.getByText('About'))
+    
+    userEvent.click(aboutTab)
+    
+    expect(history.location.pathname).toBe('/about')
+  })
+
+  it('should route user to Find a Roommate page when Find A Roommate tab is clicked', async () => {
+    const history = createMemoryHistory()
+
+    render(
+      <Router history={history}>
+        <NavBar/>
+      </Router>
+    )
+
+    const roommateTab = await waitFor(() => screen.getByText('Find A Roommate'))
+    
+    userEvent.click(roommateTab)
+    
+    expect(history.location.pathname).toBe('/find-a-roommate')
   })
 })

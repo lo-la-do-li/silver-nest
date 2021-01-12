c8708a91b94cd1696567fa0db2d4856fed7e80d4
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-12 11:56:08 -0700

    "Create mock data for residents"

---
+++ src/mockResidentData.js
@@ -0,0 +1,55 @@
+const mockResidents = [
+  {
+    name: "Doris",
+    photo: "https://erinwritesnow.files.wordpress.com/2017/01/cute-happy-older-lady.jpg",
+    age: "87",
+    height: "5'9\"",
+    weight: "145 lbs",
+    pcp: "Dr. Uppitomison",
+    ambulatory: "yes",
+    help_with_medications: "yes",
+    other_impairments: "visual impairment",
+    previous_career: "Seamstress",
+    interests: "knitting, cats, card games, unsolved mysteries",
+    pets: "one cat",
+    living_preferences: "I would feel most comfortable sharing my home with a female student",
+    semester: "Spring-2021",
+    additional_notes: "I need help feeding my cat, Mittens. Please don't eat peanuts around me, I am terribly allergic."
+  },
+  {
+    name: "Ralph",
+    photo: "https://previews.123rf.com/images/natakot/natakot1810/natakot181001281/110094284-happy-old-man-showing-thumbs-up-working-out-with-a-dumbbell.jpg",
+    age: "75",
+    height: "5'11\"",
+    weight: "190 lbs",
+    pcp: "Dr. Segura",
+    ambulatory: "yes",
+    help_with_medications: "no",
+    other_impairments: "early stage memory loss",
+    previous_career: "Small business owner",
+    interests: "Weightlifting, billiards, NY stock exchange",
+    pets: "none",
+    living_preferences: "I like to wake up early and stay active.",
+    semester: "Summer-2021",
+    additional_notes: "Once a month, my 12-year-old grandson comes to stay with me. He's a good kid and is fine sleeping on the couch."
+  },
+  {
+    name: "Wendy",
+    photo: "https://reengage.org.uk/site/assets/files/1068/possible_home_page.750x0.jpg",
+    age: "85",
+    height: "5'5\"",
+    weight: "125 lbs",
+    pcp: "Dr. Green",
+    ambulatory: "no",
+    help_with_medications: "yes",
+    other_impairments: "multiple sclerosis",
+    previous_career: "Elementary School Teacher",
+    interests: "Ping-pong, walks around the block, and chinese chess",
+    pets: "dog",
+    living_preferences: "I'm an early riser, and try to get most of my errands done first thing",
+    semester: "Fall-2021",
+    additional_notes: "It's important for me to keep cooking -- it's one of my favorite hobbies -- however, I sometimes forget I have stuff on the stove"
+  }
+]
+
+export default mockResidents;

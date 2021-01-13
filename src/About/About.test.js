2e8eb1b83ed01b692596679de44cd2b82cd2668e
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-12 21:29:13 -0700

    Write and pass test for About

--- src/About/About.test.js
+++ src/About/About.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import About from './About';
 import { Router, MemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from "history";
 
 describe('About', () => {
   it('It should display a description of the app and the collaboration', () => {
@@ -17,5 +18,20 @@ describe('About', () => {
     const missionStatement = screen.getByText('Turing School of Health Sciences has partnered with Mountain View Residences, a senior independent living resort, to match qualified Health Sciences Students to elderly residents living in the community based on compatibility of living preferences, needs, and schedules.')
     
     expect(missionStatement).toBeInTheDocument()
+  })
+
+  it('should change have a button that a user can click to go to FindARoommate', async () => {
+    const history = createMemoryHistory()
+    render(
+      <Router history={history}>
+        <About/>
+      </Router>
+    )
+    
+    const roommateButton = await waitFor(() => screen.getByRole('button', { name: /find a roommate/i }))
+    
+    userEvent.click(roommateButton)
+    
+    expect(history.location.pathname).toBe('/find-a-roommate')
   })
 })

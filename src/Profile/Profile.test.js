529105f4d6f18ef0e981b860ae1564df00fd8264
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-12 21:39:38 -0700

    tests skipped in progress

--- src/Profile/Profile.test.js
+++ src/Profile/Profile.test.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Profile from './Profile';
-import { Router, MemoryRouter } from 'react-router';
+import ResidentCard from './Profile';
+import { MemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
+import { springResident } from '../mockResidentData';
 
 describe('Profile', () => {
-  //This test file will mostly be for unit testing
-  it.skip('It should display a profile of an resident', () => {
-    // setup and execution 
-    render(
 
+  it.skip('should display a profile of an resident', async () => {
+    render(
+      <MemoryRouter>
+        <ResidentCard
+          resident={springResident}
+          exitProfileView={jest.fn()}
+        />
+      </MemoryRouter>
     )
-    //execution
-      
-    // assertions
-    expect(screen.getByText()).toBeInTheDocument()
+    
+    const image = await waitFor(() => screen.getByTitle(/doris's photo/i))
+    const interests = screen.getByText(/knitting, cats, card games, unsolved mysteries/i)
+    const applyButton = screen.getByRole('button', { name: /apply for housing/i })
+    
+    expect(image).toBeInTheDocument()
+    expect(interests).toBeInTheDocument()
+    expect(applyButton).toBeInTheDocument()
+  })
+
+  it.skip('should allow user to view additional settings by clicking carrot icon', () => {
+    render(
+      <MemoryRouter>
+        <ResidentCard
+          resident={springResident}
+          exitProfileView={jest.fn()}
+        />
+      </MemoryRouter>
+    )
+
+    const showMore = screen.getByRole('button', { name: /show more/i })
+    const height = screen.queryByRole('heading', { name: /height: 5' 9"/i })
+
+    expect(height).not.toBeInTheDocument()
+
+    userEvent.click(showMore)
+
+    expect(height).toBeInTheDocument()
   })
 })

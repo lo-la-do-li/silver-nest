e2bd16758f5e38e0bba7d5b1173f099f52757275
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2021-01-12 19:58:58 -0700

    Edit thumbnails testing

--- src/Thumbnails/Thumbnails.test.js
+++ src/Thumbnails/Thumbnails.test.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Thumbnails from './Thumbnails';
-import { Router, MemoryRouter } from 'react-router';
-import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router';
 import { springResident } from '../mockResidentData';
 
 describe('Thumbnails', () => {
   it('should display residents', () => {
-    // setup and execution 
     render(
       <MemoryRouter>
         <Thumbnails
@@ -18,9 +16,9 @@ describe('Thumbnails', () => {
         />
       </MemoryRouter>
     )
-    //execution
+
     const doris = screen.getByRole('img', { name: /doris/i })
-    // assertions
+
     expect(doris).toBeInTheDocument()
   })
 })

e1e4b1b1b3f7ad8c82489ade437411aee61fef3c
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-24 16:57:24 -0700

    Add styles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { CssBaseline } from '@material-ui/core'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App/App";
+import reportWebVitals from "./reportWebVitals";
+// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <CssBaseline />
+    <Router>
+      <CssBaseline />
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

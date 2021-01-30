51f0798e6b1e66d6e6917b60e19b6b7c0bb2f4a6
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-01-30 10:28:52 -0700

    Replace React logo with heart logo in head

--- src/assets/tileData.js
+++ src/assets/tileData.js
@@ -1,29 +1,33 @@
-import turingHealthLogo from '../turingHealthLogo.png';
+import turingHealthLogo from "../turingHealthLogo.png";
 
 export const tileData = [
   {
     img: turingHealthLogo,
     title: "image1",
-    cols: 1
-  },
-   {
-    img: "https://d354o3y6yz93dt.cloudfront.net/uploads/455b4af16646b8f1bbe60b843070b1a0/MorningStarofBoulder_Photos_04_Seniorly.jpg",
-    title: "image5",
-    cols: 2
-  },
-   {
-    img: "https://c8.alamy.com/comp/JAJ1TK/students-and-seniors-making-small-talk-in-workshop-break-JAJ1TK.jpg",
-    title: "image3",
-    cols: 3
-  },
-   {
-    img: "https://ak.picdn.net/offset/photos/5f6d0e80a75ca0db370a31bc/medium/offset_1009282.jpg",
-    title: "image2",
-    cols: 2
+    cols: 1,
   },
   {
-    img: "https://c8.alamy.com/comp/D51WWG/senior-klara-fuerst-78-and-student-sarah-boehm-20-cut-fruit-at-the-D51WWG.jpg",
-    title: "image4",
-    cols: 1
+    img:
+      "https://d354o3y6yz93dt.cloudfront.net/uploads/455b4af16646b8f1bbe60b843070b1a0/MorningStarofBoulder_Photos_04_Seniorly.jpg",
+    title: "image5",
+    cols: 2,
   },
-]
+  {
+    img:
+      "https://c8.alamy.com/comp/JAJ1TK/students-and-seniors-making-small-talk-in-workshop-break-JAJ1TK.jpg",
+    title: "image3",
+    cols: 3,
+  },
+  {
+    img:
+      "https://ak.picdn.net/offset/photos/5f6d0e80a75ca0db370a31bc/medium/offset_1009282.jpg",
+    title: "image2",
+    cols: 2,
+  },
+  {
+    img:
+      "https://c8.alamy.com/comp/D51WWG/senior-klara-fuerst-78-and-student-sarah-boehm-20-cut-fruit-at-the-D51WWG.jpg",
+    title: "image4",
+    cols: 1,
+  },
+];
